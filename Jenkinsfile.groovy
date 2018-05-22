7c6f92a7afd52f2bd5cdec42561d73cba58b4161
Rob <brem_robert@hotmail.com>
2018-05-22 15:03:26 +0200

    [BUG] another try :-)

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -42,47 +42,19 @@ podTemplate(label: 'mypod', containers: [
                 } catch (Exception e) {
                     echo 'no keycloak-export-text.json found'
                 }
+                sh 'mkdir backup-repo'
+                sh 'cd backup-repo'
                 sh "${kct} cp ${podName}:/opt/jboss/keycloak-export.json ./keycloak-export-test.json"
             }
             sh 'git config user.email "jenkins@khinkali.ch"'
             sh 'git config user.name "Jenkins"'
+            sh 'git add --all .'
+            sh "git diff --quiet && git diff --staged --quiet || git commit -am 'new_version'"
+
             withCredentials([usernamePassword(credentialsId: 'bitbucket', passwordVariable: 'GIT_PASSWORD', usernameVariable: 'GIT_USERNAME')]) {
-                commitAndPushRepo()
+                sh "git push https://${GIT_USERNAME}:${GIT_PASSWORD}@bitbucket.org/khinkali/jenkins_backup"
             }
         }
 
-        stage('create backup from production') {
-            def kc = 'kubectl'
-            container('kubectl') {
-                def keycloakPods = sh(
-                        script: "${kc} get po -l app=keycloak",
-                        returnStdout: true
-                ).trim()
-                def podNameLine = keycloakPods.split('\n')[1]
-                def startIndex = podNameLine.indexOf(' ')
-                if (startIndex == -1) {
-                    return
-                }
-                def podName = podNameLine.substring(0, startIndex)
-                sh "${kc} exec ${podName} -- /opt/jboss/keycloak/bin/standalone.sh -Dkeycloak.migration.action=export -Dkeycloak.migration.provider=singleFile -Dkeycloak.migration.file=keycloak-export.json -Djboss.http.port=5889 -Djboss.https.port=5998 -Djboss.management.http.port=5779 &"
-                sleep 60
-                git(
-                        url: 'https://bitbucket.org/khinkali/keycloak_backup',
-                        credentialsId: 'bitbucket')
-                sh 'rm keycloak-export-prod.json'
-                sh "${kc} cp ${podName}:/opt/jboss/keycloak-export.json ./keycloak-export-prod.json"
-            }
-            sh 'git config user.email "jenkins@khinkali.ch"'
-            sh 'git config user.name "Jenkins"'
-            withCredentials([usernamePassword(credentialsId: 'bitbucket', passwordVariable: 'GIT_PASSWORD', usernameVariable: 'GIT_USERNAME')]) {
-                commitAndPushRepo()
-            }
-        }
     }
-}
-
-def commitAndPushRepo() {
-    sh "git add --all"
-    sh "git diff --quiet && git diff --staged --quiet || git commit -am 'new_version'"
-    sh "git push https://${GIT_USERNAME}:${GIT_PASSWORD}@bitbucket.org/khinkali/keycloak_backup"
 }
